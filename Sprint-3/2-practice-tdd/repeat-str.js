bea11a55133f01ec0e8b1a66c504727aa924dcd3
pathywang <pathywang@gmail.com>
2026-02-15 22:57:47 +0000

    commit

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,11 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str,count) {
+  if (count>0) return str.repeat(count)
+  if (count===0) return ""
+  if (count<0) return"invalid count"
 }
+
+console.log(repeatStr("hello",5))
+console.log(repeatStr("nihao",1))
+console.log(repeatStr("nihao",-5))
 
 module.exports = repeatStr;
