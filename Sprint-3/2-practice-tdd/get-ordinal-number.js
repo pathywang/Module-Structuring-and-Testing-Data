879957bf45e938a3dd29f5d112f84b209ec53cce
pathywang <pathywang@gmail.com>
2026-02-15 20:23:48 +0000

    pass test

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,25 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  lastDigit = num % 10;
+  lastTwoDigits = num % 100;
+
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+    return num + "th";
+  }
+
+  switch (lastDigit) {
+    case 1:
+      return num + "st";
+    case 2:
+      return num + "nd";
+    case 3:
+      return num + "rd";
+    default:
+      return num + "th";
+  }
 }
+
+console.log(getOrdinalNumber(112))
+
+
 
 module.exports = getOrdinalNumber;
