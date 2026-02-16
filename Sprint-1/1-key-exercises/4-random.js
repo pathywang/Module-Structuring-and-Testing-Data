1177a65e4399bce5a71e8c9ce9ead777b33a48c5
pathywang <pathywang@gmail.com>
2026-02-16 07:03:26 +0000

    correction

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -3,15 +3,17 @@ const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
+console.log(num)
+
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it mean
 // It will help to think about the order in which expressions are evaluated
 
-// Try logging the value of num and running the program several times to build an idea of what the program is doingnum reprents the number is  randomly generated whole number betweeb 1 and 100,inclusive range is between 0 and 99.99999
+// Try logging the value of num and running the program several times to build an idea of what the program is doing.
 
-//Math.random() returns a random decimal between 0(inclusive) and 1(exclusive). while maximum-minimum +1=100, Math.random()*(maximum-minimum+1)
-
-//Due to that Math.floor(...) rounds down the decimal to the nearest whole number.From this Stage, we have interger between o and 99.After + minimum, the range is between 1 and 100.
-//This is classcial expressions for generating a random integer between two values- between 1 and 100, inclusive in this function.
+//Math.random() returns a random decimal between 0(inclusive) and 1(exclusive). while maximum-minimum +1=100,+1 is important — it makes the range inclusive (so 100 is possible).
+//Math.floor(...) rounds down the decimal to the nearest whole number.From this Stage, we have integer between o and 99.After + minimum, the range is between 1 and 100.
+//num represents a random whole number between 1 and 100 (inclusive).It is the standard way to generate a random integer between min and max inclusive.
+//This is classical expressions for generating a random integer between two values- between 1 and 100, inclusive in this function.
 //This kind of expression is commonly used for things like: rolling dice,random quiz questions, game mechanics.
--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,4 +1,4 @@
 // This is just an instruction for the first activity - but it is just for human consumption
 // We don't want the computer to run these 2 lines - how can we solve this problem?
 
-In JavaScript, we put // in front of sentence which is only for human to read.
+//In JavaScript, we put // in front of sentence which is only for human to read.
