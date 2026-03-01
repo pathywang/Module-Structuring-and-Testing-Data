0f15af06e8fd79876250d668be0e9c0dd9759a54
pathywang <pathywang@gmail.com>
2026-03-01 20:19:12 +0000

    commit

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,9 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, count) {
+  if (count < 0) {
+    throw new Error("Count cannot be negative");
+  }
+
+  return str.repeat(count);
 }
 
 module.exports = repeatStr;
